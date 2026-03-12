1dd15e042ce342a29194be28c7bcff7211fd06d6
Santiago <123smj123@gmail.com>
2026-03-12 19:31:06 +0100

    Added NeDB on Deaths-By-Risk-Factors

--- src/routes/deaths-by-risk-factors.js
+++ src/routes/deaths-by-risk-factors.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const store = new dataStore();
 
-const data = [{"a": "value_a"}, {"b": "value_b"}, {"c": "value_c"}];
+const data = [];
 
 store.insert(data);
 
@@ -18,7 +18,10 @@ router.get('/deaths-by-risk-factors/docs', (req, res) => {
 
 // Loads the data from the file and stores it in memory for the route
 router.get('/deaths-by-risk-factors/loadInitialData', (req, res) => {
-  if (store.deathsByRiskFactors.length === 0) {
+  store.count({}, (err, count) => {
+    if (count > 0) {
+      return res.status(409).send("Conflict: Data already loaded");
+    }
     let data = readFile('number-of-deaths-by-risk-factor.csv');
     let filteredData = data.map(item => ({
       Entity: item.Entity,
@@ -28,13 +31,12 @@ router.get('/deaths-by-risk-factors/loadInitialData', (req, res) => {
       'Child wasting': item['Child wasting'],
       'Household air pollution from solid fuels': item['Household air pollution from solid fuels'],
       'High fasting plasma glucose': item['High fasting plasma glucose']
-    })
-    );
-    store.deathsByRiskFactors = filteredData.slice(0, 10);
-    return res.status(200).json(store.deathsByRiskFactors);
-  } else {
-    return res.status(409).send('Conflict: Data already loaded');
-  }
+    }));
+    store.insert(filteredData.slice(0, 10), (err, docs) => {
+      docs.forEach(d => delete d._id);
+      res.status(201).json(docs);
+    });
+  });
 });
 
 // Returns the data stored in memory for the route
@@ -51,20 +53,17 @@ router.get('/deaths-by-risk-factors', (req, res) => {
 // Creates a new entry in the data stored in memory for the route
 router.post('/deaths-by-risk-factors', (req, res) => {
   const newData = req.body;
-  if (!newData) {
-    return res.status(400).send('Bad request: No data provided');
-  }
-  if (newData.Entity === undefined || newData.Year === undefined || newData['High systolic blood pressure'] === undefined ||
-    newData['Air pollution'] === undefined || newData['Child wasting'] === undefined ||
-    newData['Household air pollution from solid fuels'] === undefined ||
-    newData['High fasting plasma glucose'] === undefined) {
-    return res.status(400).send('Bad request: Incomplete data provided');
-  }
-  if (store.deathsByRiskFactors.some(item => item.Entity.toLowerCase() === newData.Entity.toLowerCase() && item.Year === newData.Year)) {
-    return res.status(409).send('Conflict: Data for country already exists');
-  }
-  store.deathsByRiskFactors.push(newData);
-  res.status(201).json(newData);
+  store.findOne(
+    { Entity: newData.Entity, Year: newData.Year },
+    (err, doc) => {
+      if (doc)
+        return res.status(409).send("Conflict: Data already exists");
+      store.insert(newData, (err, inserted) => {
+        delete inserted._id;
+        res.status(201).json(inserted);
+      });
+    }
+  );
 });
 
 // Method not allowed for the route, since we don't want to update all the data at once
@@ -74,19 +73,20 @@ router.put('/deaths-by-risk-factors', (req, res) => {
 
 // Deletes all the data stored in memory for the route
 router.delete('/deaths-by-risk-factors', (req, res) => {
-  store.deathsByRiskFactors = [];
-  res.status(204).send("All data deleted");
+  store.remove({}, { multi: true }, (err, numRemoved) => {
+    res.status(204).send();
+  });
 });
 
 // Returns the data stored in memory for a specific country
 router.get('/deaths-by-risk-factors/:country', (req, res) => {
   const country = req.params.country;
-  const data = store.deathsByRiskFactors.filter(item => item.Entity.toLowerCase() === country.toLowerCase());
-  if (data.length > 0) {
+  store.find({ Entity: new RegExp(`^${country}$`, "i") }, (err, data) => {
+    if (data.length === 0)
+      return res.status(404).send("Data not found");
+    data.forEach(d => delete d._id);
     res.status(200).json(data);
-  } else {
-    res.status(404).send('Data not found for country: ' + country);
-  }
+  });
 });
 
 // Method not allowed for the route, since we don't want to create a new country with a specific country
@@ -96,44 +96,29 @@ router.post('/deaths-by-risk-factors/:country', (req, res) => {
 
 // Updates the data stored in memory for a specific country
 router.put('/deaths-by-risk-factors/:country', (req, res) => {
-  const country = req.params.country.toLowerCase();
-
-  if (!req.body) {
-    return res.status(400).send('Bad request: No data provided');
-  }
-  if (req.body.Entity === undefined || req.body.Year === undefined || req.body['High systolic blood pressure'] === undefined ||
-    req.body['Air pollution'] === undefined || req.body['Child wasting'] === undefined ||
-    req.body['Household air pollution from solid fuels'] === undefined ||
-    req.body['High fasting plasma glucose'] === undefined) {
-    return res.status(400).send('Bad request: Incomplete data provided');
-  }
-  if (req.body.Entity.toLowerCase() !== country) {
-    return res.status(400).send('Bad request: Country in the body does not match the country in the URL');
-  }
-
-  const index = store.deathsByRiskFactors.findIndex(item => item.Entity.toLowerCase() === country);
-
-  if (index === -1) {
-    return res.status(404).send('Data not found for country: ' + country);
-  }
-
-  store.deathsByRiskFactors[index] = req.body;
-
-  res.status(200).json(store.deathsByRiskFactors[index]);
+  const country = req.params.country;
+  if (req.body.Entity.toLowerCase() !== country.toLowerCase())
+    return res.status(400).send("Country mismatch");
+  store.update(
+    { Entity: country },
+    req.body,
+    {},
+    (err, numUpdated) => {
+      if (numUpdated === 0)
+        return res.status(404).send("Data not found");
+      res.status(200).json(req.body);
+    }
+  );
 });
 
 // Deletes the data stored in memory for a specific country
 router.delete('/deaths-by-risk-factors/:country', (req, res) => {
-  const country = req.params.country.toLowerCase();
-
-  const index = store.deathsByRiskFactors.findIndex(item => item.Entity.toLowerCase() === country);
-
-  if (index === -1) {
-    return res.status(404).send('Data not found for country: ' + country);
-  }
-
-  store.deathsByRiskFactors.splice(index, 1);
-  res.status(204).end();
+  const country = req.params.country;
+  store.remove({ Entity: country }, {}, (err, numRemoved) => {
+    if (numRemoved === 0)
+      return res.status(404).send("Data not found");
+    res.status(204).send();
+  });
 });
 
 export default router;
