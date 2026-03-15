9a2ea06da24a06da2a4ced4170b1f2b3a9dac028
rosmery21 <rosmerymarculli@gmail.com>
2026-03-16 00:16:32 +0100

    Added GET POST DELETE endpoints

--- src/routes/child-malnutritions.js
+++ src/routes/child-malnutritions.js
@@ -3,10 +3,28 @@ const router = express.Router();
 
 let childMalnutritions = [];
 
+// GET all data
 router.get("/child-malnutritions", (req, res) => {
     res.send(childMalnutritions);
 });
 
+// GET single data
+router.get("/child-malnutritions/:country/:year",(req,res)=>{
+
+const country=req.params.country;
+const year=parseInt(req.params.year);
+
+const data=childMalnutritions.find(d=>d.country===country && d.year===year);
+
+if(!data){
+res.status(404).send("Not found");
+}else{
+res.send(data);
+}
+
+});
+
+// load initial data
 router.get("/child-malnutritions/loadInitialData",(req,res)=>{
 
 if(childMalnutritions.length===0){
@@ -29,6 +47,39 @@ res.status(201).send("Initial data loaded");
 }else{
 
 res.status(200).send("Data already loaded");
+
+}
+
+});
+router.post("/child-malnutritions",(req,res)=>{
+
+const newData=req.body;
+
+childMalnutritions.push(newData);
+
+res.status(201).send("Created");
+
+});
+router.delete("/child-malnutritions",(req,res)=>{
+
+childMalnutritions=[];
+
+res.status(200).send("All data deleted");
+
+});
+router.delete("/child-malnutritions/:country/:year",(req,res)=>{
+
+const country=req.params.country;
+const year=parseInt(req.params.year);
+
+const index=childMalnutritions.findIndex(d=>d.country===country && d.year===year);
+
+if(index===-1){
+res.status(404).send("Not found");
+}else{
+
+childMalnutritions.splice(index,1);
+res.status(200).send("Deleted");
 
 }
 
