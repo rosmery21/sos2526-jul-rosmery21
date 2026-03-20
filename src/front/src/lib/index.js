11f2d638eab624a5f88c705c6a10d4cdeb765724
Santiago <123smj123@gmail.com>
2026-03-20 11:04:24 +0100

    a!

--- src/front/src/lib/index.js
+++ src/front/src/lib/index.js
@@ -1,1 +1,18 @@
-// place files you want to import through the `$lib` alias in this folder.
+import apis from './data/apis.js';
+
+export function loadApisInfo() {
+    let parsedApis = `<section>` + apis.map(api => parseApi(api)).join('') + `</section>`;
+    return parsedApis;
+}
+
+function parseApi(api) {
+    return `<article>
+        <h3>${api.nombre}</h3>
+        <p>Miembro: ${api.miembro}</p>
+        <p>Recurso: ${api.recurso}</p>
+        <p>Frontend: <a href="${api.frontend}" target="_blank">${api.frontend}</a></p>
+        <p>API: <a href="${api.api}" target="_blank">${api.api}</a></p>
+        <p>Docs: <a href="${api.docs}" target="_blank">${api.docs}</a></p>
+        <p>GitHub: <a href="${api.github}" target="_blank">${api.github}</a></p>
+    </article>`;
+}
--- src/front/src/routes/+layout.svelte
+++ src/front/src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import Header from '$lib/components/Header.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<Header />
 
 {@render children()}
