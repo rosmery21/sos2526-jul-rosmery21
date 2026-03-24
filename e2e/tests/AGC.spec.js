1eeedc0acdbb9ff10f0381e56335036cfd8c906f
alexia <alegutcas@alum.us.es>
2026-03-24 18:49:21 +0100

    D02

---
+++ e2e/tests/AGC.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('El frontend carga la lista de pandemias correctamente', async ({ page }) => {
+  await page.goto('http://localhost:5173/pandemics'); 
+
+  const botonAdd = page.locator('text=Añadir nuevo dato');
+  await expect(botonAdd).toBeVisible();
+});
